1b238321c0e4a0e04538e204f56071c41677c2e4
breno morais <breno.lopes.morais@hotmail>
2018-08-25 17:27:47 -0300

    configurando para deploy externo em container e server de app

--- src/main/java/br/com/brenomorais/webapp/WebappApplication.java
+++ src/main/java/br/com/brenomorais/webapp/WebappApplication.java
@@ -2,15 +2,21 @@ package br.com.brenomorais.webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class WebappApplication {
+	
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WebappApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebappApplication.class, args);
+
 		System.out.println("..................");
 		System.out.println(":: WebApp Start ::");
 		System.out.println("..................");
-
 	}
+
 }
