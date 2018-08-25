e2993fbe185e50692a5d4524f228d6695db5b12c
breno morais <breno.lopes.morais@hotmail>
2018-08-25 03:20:20 -0300

    Adicionando dependencia  do thymeleaf-layout-dialect necessário para Spring boot 2, codificando pagina home

--- src/main/java/br/com/brenomorais/webapp/controller/MainController.java
+++ src/main/java/br/com/brenomorais/webapp/controller/MainController.java
@@ -1,14 +1,35 @@
 package br.com.brenomorais.webapp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MainController {
 	
-	//Método carrega a pagina index.html de login do sistema
-	
+	/*
+	 * Método carrega a pagina index.html de login do sistema
+	 */	
+	@RequestMapping(value="/", method = RequestMethod.GET)
 	public String index() {
 		return "index";
+	}
+	
+	
+	/*
+	 * Redireciona para à pagina home após o login
+	 */	
+	@RequestMapping(value="/home", method = RequestMethod.GET)
+	public String home() {
+		return "home";
+	}
+	
+	/*
+	 * Redireciona quando o usuário não tive permissão para alguma função
+	 */		
+	@RequestMapping(value="/acessoNegado", method= RequestMethod.GET)
+	public String acessoNegado(){ 
+		return "acessoNegado";
 	}
 
 }
