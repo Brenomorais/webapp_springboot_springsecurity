f300ed2b9839de253acb8949c80104b70e6ed28b
breno morais <breno.lopes.morais@hotmail>
2018-08-25 23:45:55 -0300

    correção executar webapp no tomcat interno e externo

--- src/main/java/br/com/brenomorais/webapp/WebappApplication.java
+++ src/main/java/br/com/brenomorais/webapp/WebappApplication.java
@@ -3,10 +3,12 @@ package br.com.brenomorais.webapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WebappApplication {
+public class WebappApplication extends SpringBootServletInitializer{
 	
+	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(WebappApplication.class);
 	}
